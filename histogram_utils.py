3dfa7f3600ac9361e2e39fb88e4d8ef9b2711eb4
shirblum1 <shirblum1@gmail.com>
2025-11-20 13:40:58 +0200

    Update histogram_utils.py

--- histogram_utils.py
+++ histogram_utils.py
@@ -10,7 +10,7 @@ def build_histogram(data):
     return my_dict
 
 def plot_histogram(histogram):
-    import matplotlib.pyp as plt
+    import matplotlib as plt
     x_values = list(histogram.keys())
     y_values = list(histogram.values())
 
